2b0ada6c1a49a38070987980d081fb0437b05bb7
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-01 18:32:20 +0100

    feat: improve compute readability by moving security parts in

---
+++ modules/ec2_instance/security.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_htpp"
+  description = "Allow http inbound and outbound traffic"
+  vpc_id      = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
