8267bed91c03317fc0874541fa9828a4ef4dcbcc
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-02 20:54:32 +0100

    feat: format main with all ressources

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
 
+  project_name = var.project_name
+
+  instance_type     = var.instance_type
+  vpc_cidr          = var.vpc_cidr
+  subnet_cidr_block = var.subnet_cidr_block
+}
