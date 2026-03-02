c14e34bbb271ad8fbcfba1aae94b2332c9c667f9
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-02 20:55:35 +0100

    refactor: clean variables

--- modules/ec2_instance/vars.tf
+++ modules/ec2_instance/vars.tf
@@ -1,17 +1,19 @@
 variable "instance_type" {
   description = "Select instance type"
   type        = string
-  default     = "t3.micro"
 }
 
 variable "subnet_cidr_block" {
   description = "select subnet cidr"
   type        = string
-  default     = "172.10.0.0/28"
 }
 
 variable "vpc_cidr" {
   description = "select vpc cidr"
   type        = string
-  default     = "172.10.0.0/24"
+}
+
+variable "project_name" {
+  description = "project's name"
+  type        = string
 }
