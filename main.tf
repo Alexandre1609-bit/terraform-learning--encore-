f599f041226bf126bf3478510c23f896fa166da5
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-10 11:27:10 +0100

    refactor: add var dependencies, fix typo

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-
-  project_name = var.project_name
-
-  instance_type     = var.instance_type
+module "network" {
+  source            = "./modules/network"
   vpc_cidr          = var.vpc_cidr
   subnet_cidr_block = var.subnet_cidr_block
+}
+
+module "compute" {
+  source        = "./modules/compute"
+  project_name  = var.project_name
+  instance_type = var.instance_type
+
+  subnet_id         = module.network.subnet_id
+  security_group_id = module.network.security_group_id
 }
