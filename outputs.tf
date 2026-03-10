3b52d06d4871f2a6b8e5304152b6ad8b5be5c77f
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-10 12:29:18 +0100

    fix: wrong outputs path

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 
 output "ec2_instance_id" {
   description = "id of the ec2 instance"
-  value       = module.ec2_instance.ec2_instance_id
+  value       = module.compute.ec2_instance_id
 }
 
 output "ec2_instance_ip" {
   description = "ec2 instance's private ip"
-  value       = module.ec2_instance.ec2_instance_ip
+  value       = module.compute.ec2_instance_private_ip
   sensitive   = true
 }
 
 output "vpc_id" {
   description = "vpc's id"
-  value       = module.ec2_instance.vpc_id
+  value       = module.network.vpc_id
 }
