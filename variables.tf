5e07dbd19365d4d4f87dcb21dd528eb7ffd68972
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-29 18:16:46 +0200

    feat: add ingress & egress rules's var

--- variables.tf
+++ variables.tf
@@ -26,5 +26,27 @@ variable "number_of_instance" {
     condition     = var.number_of_instance > 0 && var.number_of_instance <= 10
     error_message = "please choose a number between 1 and 10"
   }
-
 }
+
+variable "ingress_rules" {
+  description = "Ingress port to use in security group"
+  type = map(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+}
+
+variable "egress_rules" {
+  description = "Ingress port to use in security group"
+  type = map(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+}
+
