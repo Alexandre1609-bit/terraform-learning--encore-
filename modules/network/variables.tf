9ef38b11f9b2d5e175115c98062203fd3e0824b5
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-29 18:18:07 +0200

    feat: add ingress & egress var

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -2,3 +2,25 @@ variable "vpc_cidr" {
   description = "select vpc cidr"
   type        = string
 }
+
+variable "ingress_rules" {
+  description = "Ingress port to use in security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+}
+
+variable "egress_rules" {
+  description = "Ingress port to use in security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+}
