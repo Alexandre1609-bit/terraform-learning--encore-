40c06ba2f02ca77fc797686218ff1c493c07d9a0
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-04-14 10:44:44 +0200

    feat(wip): add iam policies module & and add custom policy to replace & enhance the older one

--- modules/iam_policies/test_iam.tf
+++ modules/iam_policies/test_iam.tf
@@ -10,15 +10,30 @@ resource "aws_iam_role" "my_first_role" {
   }
 }
 
+resource "aws_iam_policy" "restrict_bucket" {
+  name        = "restrict_bucket"
+  description = "restrict S3 bucket: read-only"
+
+  policy = {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:ListBucket"]
+    resource = [
+      "arn:aws:s3:::my-first-bucket-test-1609",
+      "arn:aws:s3:::my-first-bucket-test-1609/*"
+    ]
+  }
+}
+
 resource "aws_iam_role_policy_attachment" "test_iam_attachment" {
   role       = aws_iam_role.my_first_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  policy_arn = aws_iam_policy.restrict_bucket.arn
 }
 
 resource "aws_iam_instance_profile" "iam_test_profil" {
   name = "test_profil"
   role = aws_iam_role.my_first_role.name
 }
+
 
 data "aws_iam_policy_document" "example_policy" {
 
@@ -39,4 +54,18 @@ data "aws_iam_policy_document" "example_policy" {
 
   }
 
+}
+
+data "aws_iam_policy_document" "restrict_s3_policy_doc" {
+  statement {
+    sid = "RestrictS3Policy"
+
+    actions = ["sts::Assumerole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    effect = "Allow"
+  }
 }
